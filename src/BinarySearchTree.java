b8f10be7620b46e0be16430676a36bcb5c93e55a
Hansatang <portokarlos@gmail.com>
2022-04-04 11:45:18 +0200

    T in Methods

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -55,45 +55,44 @@ public class BinarySearchTree<T extends Comparable<T>> extends BinaryTree<T> {
         return delete(root, data) != null ? true : false;
     }
 
-    private BinarySearchTreeNode delete(BinarySearchTreeNode checkedNode, T data) {
+    private BinarySearchTreeNode<T> delete(BinarySearchTreeNode<T> checkedNode, T data) {
         System.out.println("CheckedNode "+checkedNode.getElement());
         if (checkedNode == null) {
             return checkedNode;
         } else if (checkedNode.getElement().compareTo(data) > 0) {
-            checkedNode.left = delete((BinarySearchTreeNode) checkedNode.getLeftChild(), data);
+            checkedNode.left = delete((BinarySearchTreeNode<T>) checkedNode.getLeftChild(), data);
         } else if (checkedNode.getElement().compareTo(data) < 0) {
-            checkedNode.right = delete((BinarySearchTreeNode) checkedNode.getRightChild(), data);
+            checkedNode.right = delete((BinarySearchTreeNode<T>) checkedNode.getRightChild(), data);
         } else {
             if (checkedNode.left == null) {
-                return (BinarySearchTreeNode) checkedNode.getRightChild();
+                return (BinarySearchTreeNode<T>) checkedNode.getRightChild();
             } else if (checkedNode.right == null) {
-                return (BinarySearchTreeNode) checkedNode.getLeftChild();
+                return (BinarySearchTreeNode<T>) checkedNode.getLeftChild();
             }
-            checkedNode.value = findMin((BinarySearchTreeNode) checkedNode.right);
-            checkedNode.right = delete((BinarySearchTreeNode) checkedNode.right, (T) checkedNode.getElement());
+            checkedNode.value = findMin((BinarySearchTreeNode<T>) checkedNode.right);
+            checkedNode.right = delete((BinarySearchTreeNode<T>) checkedNode.right, (T) checkedNode.getElement());
         }
         return checkedNode;
     }
 
 
-    public int findMin(BinarySearchTreeNode node) {
-        int min = (int) node.getElement();
+    public T findMin(BinarySearchTreeNode<T> node) {
+        T min =  node.getElement();
 
 
         while (node.getLeftChild() != null) {
-            min = (int) node.getLeftChild().getElement();
-            node = (BinarySearchTreeNode) node.getLeftChild();
+            min =  node.getLeftChild().getElement();
+            node = (BinarySearchTreeNode<T>) node.getLeftChild();
         }
         return min;
     }
 
-    public int findMax(BinarySearchTreeNode node) {
-        int max = (int) node.getElement();
-
+    public T findMax(BinarySearchTreeNode<T> node) {
+        T max =  node.getElement();
 
         while (node.getRightChild() != null) {
-            max = (int) node.getRightChild().getElement();
-            node = (BinarySearchTreeNode) node.getRightChild();
+            max =  node.getRightChild().getElement();
+            node = (BinarySearchTreeNode<T>) node.getRightChild();
         }
         return max;
     }
